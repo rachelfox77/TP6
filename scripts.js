d01a02e8fa794dab93c8bf09aadc8c29082ba4bb
rachel <rachel.fox@wsu.edu>
2021-10-16 20:21:30 +0000

    first upload of project files

---
+++ scripts.js
@@ -0,0 +1,30 @@
+window.onload=function(){
+  // $('H1').css({'text-color': 'red'}); 
+  
+  document.querySelector("h1").style.color = "red";
+}
+
+
+
+function myFunction() {
+  document.getElementById("ingredients").innerHTML = "ingredients";
+}
+
+
+function myFunction() {
+  document.getElementById("equipment").innerHTML = "equipment";
+}
+
+
+function myFunction() {
+  document.getElementById("directions").innerHTML = "directions";
+}
+
+
+document.getElementById("serving size").innerHTML = "Serving Size Approx 12" ;
+
+document.getElementById("calories").innerHTML = 1200 / 12
+
+
+  
+  
