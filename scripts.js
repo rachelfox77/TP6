226c30c0c0a33369a137cbd635b705a9e0bebd08
rachel <rachel.fox@wsu.edu>
2021-10-16 21:23:40 +0000

    fixed all java

--- scripts.js
+++ scripts.js
@@ -1,29 +1,43 @@
-window.onload=function(){
+
+
+
+window.onload = function() {
+  
+  window.onload=function(){
   // $('H1').css({'text-color': 'red'}); 
   
-  document.querySelector("h1").style.color = "red";
+  document.querySelector("#CrepeHeader").style.color = "red";
 }
 
+  
+  document.querySelector("#crepeHeader").classList.add ("tp6");
+  
+  document.querySelector("#ingredients").onclick = function() {
+    document.querySelector("#ingredients ul").classList.toggle("showMe");
+  }
+  
+  document.querySelector("#equipment").onclick = function() {
+    document.querySelector("#equipment ul").classList.toggle("showMe");
+  }
+  
+  document.querySelector("#directions").onclick = function() {
+    document.querySelector("#directions ul").classList.toggle("showMe");
+  }
+  
+  document.querySelector("#crepeHeader").onclick = function(){
+    this.classList.toggle("tp6");
+  }
+  
+  document.querySelector("#footer").innerHTML += "<p>Serving Size 4 </p>";
+} // end of window.onload
 
 
-function myFunction() {
-  document.getElementById("ingredients").innerHTML = "ingredients";
-}
-
-
-function myFunction() {
-  document.getElementById("equipment").innerHTML = "equipment";
-}
-
-
-function myFunction() {
-  document.getElementById("directions").innerHTML = "directions";
-}
-
-
+  
 document.getElementById("serving size").innerHTML = "Serving Size Approx 12" ;
 
 document.getElementById("calories").innerHTML = 1200 / 12
+
+
 
 
   
